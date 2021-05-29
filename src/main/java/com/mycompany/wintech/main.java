40fd699d64baa5d87d04274d956c19c9dc03d537
Thomas <ThomasTail@users.noreply.github.com>
2021-05-29 14:24:32 -0400

    Update main.java

--- src/main/java/com/mycompany/wintech/main.java
+++ src/main/java/com/mycompany/wintech/main.java
@@ -1,7 +1,6 @@
 package com.mycompany.wintech;
 
-import com.mycompany.wintech.Goal;
-import com.mycompany.wintech.GoalList;
+import static com.mycompany.wintech.GoalList.*;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,14 +10,14 @@ public class main {
      *
      * @param args
      */
-    public main(String args[]) {
+    public static void main(String args[]) {
         Connection conn = null;
         try {
-            conn = GoalList.connect();
+            conn = connect();
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
         }
         Goal[] arr;
-        arr = GoalList.viewTable(conn);
+        arr = viewTable(conn);
     }
 }
